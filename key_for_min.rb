550760205b39e8bd118e0afe9066ae02d8c08859
Diana Utan <didiutzi@gmail.com>
2020-02-05 23:17:08 -0600

    Update key_for_min.rb

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,14 @@
 def key_for_min_value(name_hash)
   min_key = nil 
   min_value = nil 
-  name_hash.each do |key, value|
-    if min_value == nil 
-      min_value = value 
-      min_key = key
-    elsif min_value > value 
-    min_value = value 
-    min_key = key
-end
-end
-min_key
+    name_hash.each do |key, value|
+      if min_value == nil 
+        min_value = value 
+        min_key = key
+      elsif min_value > value 
+        min_value = value 
+        min_key = key
+      end
+    end
+  min_key
 end
